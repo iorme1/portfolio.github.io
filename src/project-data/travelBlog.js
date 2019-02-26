e007fe2ed2d294782cf01357a0f2a8a5645ea583
Isaac Orme <isorme1@gmail.com>
2019-02-26 09:37:55 -0800

    Edits description of travel blog app

--- src/project-data/travelBlog.js
+++ src/project-data/travelBlog.js
@@ -7,7 +7,7 @@ const travBlogData = {
   site: "https://robins-blogs.herokuapp.com/",
   src: travelBlog,
   alt: "travel-blog",
-  desc: "Blogging application for my parent's travel's to Europe. Users can sign up, comment on posts, and subscribe to get email notifications. Built with Ruby on Rails. Implements ckeditor for blog creation.",
+  desc: "Blog web application that allows users to create accounts, comment on and thumbs-up posts, reply to and thumbs-up comments, and subscribe for email notifications. Implements ckeditor for blog creation.",
 };
 
 export default travBlogData;
